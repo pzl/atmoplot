f4cd134f95f5ae1ada2ecef53e5454560971f399
pzl <dan@panzarel.la>
2017-04-03 15:49:12 -0400

    startup with first-pass polar plotting

---
+++ README.md
@@ -0,0 +1,9 @@
+AtmoPlot
+========
+
+A collection of javascript-based helper libraries for plotting scientific and weather (atmospheric) data. It uses d3.js and leverages SVG and canvas features for interactive visualizations.
+
+Polar
+-----
+
+Draws a radial Polar-Stereographic plot
---
+++ polar/polar.js
@@ -0,0 +1,118 @@
+(function(exports){
+	/* Creates a radial stereographic plot
+	 * @param container d3 element to put this plot in (like, d3.select('svg') or a g)
+	 * @param size int pixel size that the square should be. Includes axis labels, so final plot diameter will be slightly smaller
+	 *
+	 * @return object
+	 *           projection: map projection,
+	 *           group: the clipper inner-graph group to paint onto
+	 */
+	function Polar_Plot(container, size, options){
+		var default_opts = {
+			padding: 50,
+			graticule: {
+				extentMajor: [[-180,0],[180,89.999999]],
+				extentMinor: [[-180,0],[180,80.000001]],
+				stepMajor: [90,360],
+				stepMinor: [30,10]
+			},
+			labels: 28,
+			scale: 1.75,
+		};
+		options = _optmerge(default_opts, options);
+
+		var	width = size,
+			height = size,
+			radius = size/2-options.padding,
+			graph = container.append("g"), // to contain text outside of clip
+			graph_inner = graph.append("g"); //to be clipped
+
+		graph.attr("transform","translate("+options.padding+","+options.padding+")");
+
+		graph.append("circle")
+			.attr("r",radius)
+			.attr("cx",radius)
+			.attr("cy",radius)
+			.attr("class", "polar_graph_border");
+
+		container.append("clipPath")
+			.attr("id","polar_bounds")
+			.append("circle")
+				.attr("r",radius)
+				.attr("cx",radius)
+				.attr("cy",radius);
+
+		graph_inner.attr("clip-path","url(#polar_bounds)");
+
+		var projection = d3.geoStereographic()
+			.scale(radius*options.scale)
+			.translate([width/2-options.padding, height/2-options.padding])
+			.rotate([180,-90])
+			.clipAngle(181)
+			.clipExtent([[0,0], [width-options.padding,height-options.padding]])
+			.precision(.1);
+
+		var path = d3.geoPath()
+			.projection(projection);
+
+		var graticule = d3.geoGraticule()
+			.extentMajor(options.graticule.extentMajor)
+			.extentMinor(options.graticule.extentMinor)
+			.stepMajor(options.graticule.stepMajor)
+			.stepMinor(options.graticule.stepMinor)
+		graph_inner.append("path")
+			.datum(graticule)
+			.attr("class", "polar_graticule")
+			.attr("d",path);
+		graph.selectAll(".polar_lons")
+			.data(d3.range(0,360,30))
+			.enter()
+			.append("text")
+			.attr("class", "polar_lons")
+			.text(function(d){
+				if (d===0||d===180){
+					return d;
+				}
+				return (d<180)?d+"E":(180-(d-180))+"W";
+			})
+			.attr("x",function(d){ return projection([d,options.labels])[0] })
+			.attr("y",function(d){ return projection([d,options.labels])[1] });
+
+		d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, world) {
+			if (error) throw error;
+
+			graph_inner.insert("path", ".polar_graticule")
+				.datum(topojson.feature(world, world.objects.land))
+				.attr("class", "polar_land")
+				.attr("d", path);
+
+			graph_inner.insert("path", ".polar_graticule")
+				.datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+				.attr("class", "polar_boundary")
+				.attr("d", path);
+
+		});
+		this.projection = projection;
+		this.group = graph_inner;
+	}
+
+	function _optmerge(a,b) {
+		for (var attr in b) {
+			if ( typeof b[attr] === 'object' ){
+				a[attr] = _optmerge(a[attr],b[attr]);
+			} else {
+				a[attr] = b[attr];
+			}
+		}
+		return a;
+	}
+
+	/**
+	 * @todo: automatically place labels based on scale
+	 */
+
+	 exports.polar_plot = function(container,size,options) {
+	 	return new Polar_Plot(container,size,options);
+	 }
+
+})(this);
